0e270569d56261bc5c52eb30a5fb35d75052d2a2
Carlos R <jr.rombaldo@gmail.com>
2021-12-29 19:31:54 +0000

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # literature_review
 scripts to support literature review
+
+
+```
+python3 -m venv ./venv
+source ./venv/bin/activate
+pip install -r requirements.txt      
+```
---
+++ literature.py
@@ -0,0 +1,70 @@
+#!./venv/bin/python
+
+import argparse
+from pathlib import Path
+import json
+import rispy
+import re
+
+
+def write_file (content, file_name):
+    with open(file_name, 'w') as out:
+        out.write(content)
+
+def write_json(content, file_name):
+    write_file(json.dumps(content, indent=4, sort_keys=True), file_name)
+
+def write_ris(content, file_name):
+    write_file(rispy.dumps(content), file_name)
+
+def adjust_rayyan_tags(notes):
+    notes_new =[]
+    for note in notes:
+        if "RAYYAN-" in note:
+            for part in note.split("RAYYAN-"): notes_new.append(part.lstrip())
+        else:
+            notes_new.append(note)
+        
+    return notes_new
+
+
+parser = argparse.ArgumentParser(description='various scripts supporting systematic literature review')
+parser.add_argument('ris_file', metavar='RIS_file', type=str,
+                    help='take an RIS file, split comments  RAYYAN-INCLUSION, \
+                    RAYYAN-EXCLUSION-REASONS and RAYYAN-LABELS into dedicated entries. \
+                    Results are saved into a file named "parsed"')
+parser.add_argument('--json', action='store_true', help='Save JSON format instead of RIS')
+
+args = parser.parse_args()
+
+
+print (f'parsing {args.ris_file} ...')
+ris_file_path = Path(args.ris_file).absolute()
+
+with open(ris_file_path, 'r', ) as bibliography_file:
+    ris_entries = rispy.load(bibliography_file, skip_unknown_tags=False)
+    print (f'found {len(ris_entries)} bibliographies at {ris_file_path}')
+
+    for ris_entry in ris_entries:
+        print(f'processing title "{ris_entry["title"]}"')
+        ris_entry['notes'] = adjust_rayyan_tags(ris_entry['notes'])
+
+
+# adjusting inputed file name extension
+result_file = f'{re.sub("[^.]+$","", args.ris_file)}parsed.{"json" if args.json else "ris"}'
+
+result_path = Path(result_file).absolute()
+print (f'saving {len(ris_entries)} entries at {result_file} ')
+
+if args.json:
+    write_json(ris_entries, result_path)
+else:
+    write_ris(ris_entries, result_path)
+
+
+
+
+
+
+
+
