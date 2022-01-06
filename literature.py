eb3140e3029324f1b5d6d92e79779f00ba9f56a8
Carlos R <jr.rombaldo@gmail.com>
2022-01-06 15:06:10 +0000

    refactoring titles to studies

--- literature.py
+++ literature.py
@@ -32,13 +32,13 @@ class utils():
     def write_ris(content, file_name):
         utils.write_file(rispy.dumps(content), file_name)
 
-    def list_titles_from_ris(ris_file):
+    def list_studies_title_from_ris(ris_file):
         ris_entries = rispy.load(ris_file, skip_unknown_tags=False)
         titles = [ ris_entry["title"] for ris_entry in ris_entries]
         titles.sort()
         return titles
 
-    def list_titles_without_pdf_from_ris(ris_file):
+    def list_studies_without_pdf_from_ris(ris_file):
         ris_entries = rispy.load(ris_file, skip_unknown_tags=False)
         studies_with_pdf = []
         for ris_entry in ris_entries:
@@ -52,8 +52,8 @@ class utils():
         return studies_without_pdf
 
     def diff(ris_a, ris_b):
-        studies_a = set(utils.list_titles_from_ris(ris_a))
-        studies_b = set(utils.list_titles_from_ris(ris_b))
+        studies_a = set(utils.list_studies_title_from_ris(ris_a))
+        studies_b = set(utils.list_studies_title_from_ris(ris_b))
 
         print (f'found {len(studies_a)} studies in {ris_a.name} and {len(studies_b)} studies in {ris_b.name}')
 
@@ -83,30 +83,34 @@ class scholar():
         else: return None
 
     def find_studies_url_and_open_browser(ris_file, chunk_size):
-        titles = utils.list_titles_from_ris(ris_file)
+        # studies = utils.list_studies_from_ris(ris_file)
+        studies = utils.list_studies_without_pdf_from_ris(ris_file)
+        log.debug(f'found {len(studies)} studies')
 
-        # adjsuting the log format to inclide thread name with studiy title
+        # adjsuting the log format to inclide thread name with study
         logging.basicConfig(format='%(asctime)s  %(levelname)s  %(threadName)-40s   - %(message)s')
 
-        for chunk_of_titles in [titles[offs:offs+chunk_size] for offs in range(0, len(titles), chunk_size)]:
-            input(f"\n\nPress Enter to proccess a chunk of ({len(chunk_of_titles)}) studies\n\n")
-            # log.debug(f'processing a chunk of studies, size {len(chunk_of_titles)}')
+        for chunk_of_studies in [studies[offs:offs+chunk_size] for offs in range(0, len(studies), chunk_size)]:
+            input(f"\n\nPress Enter to proccess a chunk of ({len(chunk_of_studies)}) studies\n\n")
 
             with Pool(chunk_size) as scholar_pool:
-                scholar_pool.map(scholar.scholar_thread, chunk_of_titles)
+                scholar_pool.map(scholar.scholar_thread, chunk_of_studies)
     
     def scholar_thread(study_title):
         threading.current_thread().name = study_title[:37]+"..."
 
-        start = timeit.default_timer()
-        log.info (f'searching url for {study_title}')
-        title_url = scholar.find_study_url_on_google_scholar(study_title)
+        try:
+            start = timeit.default_timer()
+            log.info (f'searching url for {study_title}')
+            study_url = scholar.find_study_url_on_google_scholar(study_title)
 
-        if title_url:
-            log.info(f'opening browser, found [{title_url}] for {study_title}')
-            utils.open_browser(title_url)
-        else:
-            log.error(f'UTL not found for study {study_title}')
+            if study_url:
+                log.info(f'opening browser, found [{study_url}] for {study_title}')
+                utils.open_browser(study_url)
+            else:
+                log.error(f'UTL not found for study {study_title}')
+        except Exception as error:
+            log.error(f'Caught the following error on [{study_title}] \n {repr(error)}')
 
 class rayyan():
     def adjust_rayyan_tags(notes):
@@ -189,7 +193,7 @@ def parse_cli_args():
 
 
     # google scholar
-    scholar_parser = subparsers.add_parser('scholar', help='find titles URL from scholar and open on browser')
+    scholar_parser = subparsers.add_parser('scholar', help='find studies URL from scholar and open on browser')
     scholar_parser.add_argument('ris_file', 
         metavar='RIS_file', 
         type=argparse.FileType('r'),
@@ -222,7 +226,7 @@ if __name__ == "__main__":
     args = parse_cli_args()
 
     if args.script == "ris":
-        [log.info(title) for title in utils.list_titles_from_ris(args.ris_file)]
+        [log.info(title) for title in utils.list_studies_title_from_ris(args.ris_file)]
 
     if args.script =="rayyan":
         rayyan.parse_rayyan_metadata(args.ris_file, args.json)
